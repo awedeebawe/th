e8abd3fa63701670144502dd8cabe67692f0f14f
Lyubomir Marinov <lyubomir.tsekov@primeholding.com>
2018-02-23 07:30:44 +0200

    Initial Commit

---
+++ TechHuddle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechHuddle
+//
+//  Created by Lyubomir Marinov on 23.02.18.
+//  Copyright © 2018 http://linkedin.com/in/lmarinov/. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
